85affcfc5167bbae1392896631766e93dad39694
eva2077 <evwu77@gmail.com>
2023-09-11 22:15:55 -0600

    only let user continue if they enter in 5 letters

--- WordleStarter/Wordle.py
+++ WordleStarter/Wordle.py
@@ -11,11 +11,36 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
-    current_row=0
+    
     def enter_action(s):
-        current_row=gw.get_current_row()+1
-        gw.set_current_row(current_row)
-        gw.show_message(current_row)
+        letter_count = 0 
+        # get current row 
+        current_row = gw.get_current_row()
+        # count how many letters were entered 
+        count =0 
+        while count < 5:
+            letter = gw.get_square_letter(current_row,count)
+            if letter != " ":
+                letter_count += 1
+            count +=1
+        # Checking if user entered in 5 letters 
+        if letter_count < 5:
+            # reset letter count 
+            letter_count = 0 
+            # tell user to enter 5 letters 
+            gw.show_message("Please enter in 5 letters")
+            # reset row to the current one 
+            gw.set_current_row(N_ROWS-1)
+        elif letter_count == 5:
+            gw.show_message("Good guess!")
+            letter_count = 0 
+        else:
+            gw.show_message("Error!")
+        # prepare for next row
+        N_ROWS=gw.get_current_row()+1
+        # set the new row
+        gw.set_current_row(N_ROWS)
+
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
