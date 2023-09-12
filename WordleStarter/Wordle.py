d979a93f0ea7c0be8d6ed173888f653f3561de51
eva2077 <evwu77@gmail.com>
2023-09-11 20:31:26 -0600

    allow user to add words to mutiple rows

--- WordleStarter/Wordle.py
+++ WordleStarter/Wordle.py
@@ -11,9 +11,11 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
-
+    current_row=0
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        current_row=gw.get_current_row()+1
+        gw.set_current_row(current_row)
+        gw.show_message(current_row)
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
